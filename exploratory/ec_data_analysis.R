b6b2f33bf4a57cdc09959a92e0c15a1e65a5e376
River Norris <norri374@umn.edu>
2024-07-16 09:19:46 -0500

    Update
    added code for violin plots of field type versus all dependent variables

--- exploratory/ec_data_analysis.R
+++ exploratory/ec_data_analysis.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(readr)
 library(readxl)
 data <- read_excel(r"(C:\Users\rtjor\Downloads\soar_reeu\som_ec_nsf_reeu\preprocessed\ec_data.xlsx)")
+#box plots of field versus each dependent variable
 ggplot(data, aes(x = field, y = acclima_soil_ec_bulk, fill = factor(field))) +
   geom_boxplot() +
   labs(title = "Boxplot of Bulk EC by Field Type",
@@ -56,4 +57,24 @@ summary(two.way)
 # Fit linear model two
 lm_model_two <- lm(acclima_soil_ec_bulk ~ field + sample, data = ec_data)
 anova_table_two <- anova(lm_model_two)
-
+# violin plot of field versus each dependent variable
+ggplot(data, aes(x = field, y = acclima_soil_ec_bulk, fill = field)) +
+  geom_violin(trim = FALSE, scale = "width") +
+  geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1.0) +
+  labs(title = "Violin Plot of Bulk EC by Field", x = "Field Type", y = "Bulk EC")
+ggplot(data, aes(x = field, y = acclima_soil_ec_pore, fill = field)) +
+  geom_violin(trim = FALSE, scale = "width") +
+  geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1.0) +
+  labs(title = "Violin Plot of Pore EC by Field", x = "Field Type", y = "Pore EC")
+ggplot(data, aes(x = field, y = acclima_soil_permitivity, fill = field)) +
+  geom_violin(trim = FALSE, scale = "width") +
+  geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1.0) +
+  labs(title = "Violin Plot of Permitivity by Field", x = "Field Type", y = "Permitivity")
+ggplot(data, aes(x = field, y = acclima_soil_temperature, fill = field)) +
+  geom_violin(trim = FALSE, scale = "width") +
+  geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1.0) +
+  labs(title = "Violin Plot of Soil Temp by Field", x = "Field Type", y = "Soil Temperature")
+ggplot(data, aes(x = field, y = acclima_soil_vwc, fill = field)) +
+  geom_violin(trim = FALSE, scale = "width") +
+  geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1.0) +
+  labs(title = "Violin Plot of Volumetric Water Content by Field", x = "Field Type", y = "Soil VWC")
