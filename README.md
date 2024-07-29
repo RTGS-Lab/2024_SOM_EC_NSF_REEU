2734fb8718337114d285552f70122f40ec9c368b
norri374 <norri374@umn.edu>
2024-07-29 11:15:41 -0500

    Update
    updated the code for som_ec_analysis.R and the README.md to be more readable

--- README.md
+++ README.md
@@ -3,42 +3,39 @@ title: "README"
 output: html_document
 date: "2024-07-18"
 ---
-# **Project Title:** Soil Electrical Conductivity and Soil Organic Matter Mass Percent Correlation in Annual Tilled Corn and Soy Fields Versus a Perennial No-till Field
+# **Project Title:** Real-time Soil Health Sensing: Soil Permittivity and Soil Organic Matter Correlation in Annual Tilled Crops Versus Perennial Turf
 
-## **Project Description:** This project was built in the interest of further understanding the validity of electrical conductivity sensor data as a surrogate for soil organic matter mass percent in agricultural land as the number of farmers interested in more efficient, environmentally conscious management practices increases. It contains the data files and code associated with the data analysis and visualization for soil electrical conductivity correlation with soil organic matter at the University of Minnesota St. Paul Campus. This project was funded by the NSF sponsored SOAR-REEU program in partnership with Runck lab and GEMS Informatics. 
+
+## **Project Description:** This project was built in the interest of further understanding the validity of permittivity sensor data as a surrogate for soil organic matter mass percent in agricultural land as the number of farmers interested in more efficient, environmentally conscious management practices increases. It contains the data files and code associated with the data analysis and visualization for soil permittivity correlation with soil organic matter at the University of Minnesota St. Paul Campus. This project was funded by the NSF sponsored SOAR-REEU program in partnership with Runck lab and GEMS Informatics. 
 
 1. **Folder:** raw
 
-  	A. **Filename:** *ec_raw_data.csv*      
-      A CSV file that contains raw data of time and date of Acclima TDR 315H sensor readings of soil electrical conductivity, permittivity, temperature, volumetric water content in 60 second intervals 
+  A. **Filename:** *ec_raw_data.csv*      
+      CSV file that contains raw data of time and date of Acclima TDR 315H sensor readings of soil electrical conductivity, permittivity, temperature, volumetric water content in 60 second intervals 
+  B. **Filename:** *soil_report.pdf*   
+      A pdf file sent via email by Soil Testing Laboratory at UMN St. Paul campus that provides estimated soil texture, soil organic matter, phosphorous, and potassium levels for each sample location
       
-	  B. **Filename:** *soil_report.pdf*   
-      A pdf file sent via email by Soil Testing Laboratory at UMN St. Paul campus that provides estimated soil texture, soil organic matter, phosphorous, and potassium levels for each sample location       
 2. **Folder:** preprocessed
 
-   	A. **Filename:** *ec_data.xlsx*       
-      A CSV file containing time and date of Acclima TDR 315 sensor electrical conductivity, permittivity, temperature, volumetric water content data within sample ranges with corresponding coordinates, field type, sample and subsample number
-      
-    B. **Filename:** *som_data.xlsx*
-      An excel table that includes the relevant information pertaining to the experiment which includes the field, sample, organic matter measured in mass percent, and level in terms of low, medium and high
+  A. **Filename:** *ec_data.xlsx*       
+      CSV file containing time and date of Acclima TDR 315 sensor electrical conductivity, permittivity, temperature, volumetric water content data within sample ranges with corresponding coordinates, field type, sample and subsample number
+  B. **Filename:** *som_data.xlsx*
+        An excel table that includes the relevant information pertaining to the experiment which includes the field, sample, organic matter measured in mass percent, and level in terms of low, medium and high
 
 3. **Folder:** analysis_ready
 
-	  A. **Filename:** *som_ec_data.xlsx*
-	  
-      The two files in the preprocessed folder combined into one table
+	A. **Filename:** *som_ec_data.xlsx*
+	     The two files in the preprocessed folder combined into one table
 
 4. **Folder:** exploratory
     
-    A. **Filename:** *ec_data_analysis.R*
-    
-      Code written in RStudio that creates boxplots for each dependent variable versus field, one-way ANNOVA with field versus ec_bulk, and two-way ANNOVA with field, sample, and ec_bulk
+  A. **Filename:** *ec_data_analysis.R*
+    Code written in RStudio that creates boxplots for each dependent variable versus field, one-way ANOVA with field versus ec_bulk, and two-way ANOVA with field, sample, and ec_bulk
       
-    B. **Filename:** *som_ec_analysis.R*
+  B. **Filename:** *som_ec_analysis.R*
+    Code written to RStudio that analyzes SOM and permittivity data with regression lines and two way ANOVA
     
-      Code written to RStudio that analyzes SOM and EC data with regression lines and two way annova
-      
 **Relationship between files:**        
-Files in the preprocessed folder are cleaned datasets for SOM and EC containing measurements from the raw folder that are pertinent to the experiment. The file in the analysis_ready folder is a dataset created by combining the files in the preprocessed folder. Files in the exploratory folder are code written for the process of exploratory data visualization and analysis. 
+Files in the *preprocessed* folder are cleaned datasets for SOM and permittivity containing measurements from the *raw* folder that are pertinent to the experiment. The file in the *analysis_ready* folder is a dataset created by combining the files in the *preprocessed* folder. Files in the *exploratory* folder are code written for the process of exploratory data visualization and analysis.   
 
 
--- exploratory/som_ec_analysis.R
+++ exploratory/som_ec_analysis.R
@@ -1,78 +1,176 @@
-#load analysis_ready dataset
+#load libraries 
 library(tidyverse)
 library(readxl)
 library(car)
 library(ggplot2)
 library(readr)
 library(dplyr)
+
+#load analysis_ready dataset
 som_ec_data <- read_excel(r"(C:\Users\rtjor\Downloads\soar_reeu\som_ec_nsf_reeu\analysis_ready\som_ec_data.xlsx)")
-#checking correlation value between EC and SOM 
-head(som_ec_data)
-correlation <-cor(som_ec_data$acclima_soil_ec_bulk,som_ec_data$organic_matter)
-print(correlation)
-ggplot(som_ec_data, aes(x = acclima_soil_ec_bulk, y = organic_matter)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE) +
-  labs(title = "Regression plot of SOM and Bulk EC", x = "Bulk EC", y = "Soil Organic Matter (%)")
-# checking correlation value between EC Pore and SOM
-correlation_pore <-cor(som_ec_data$acclima_soil_ec_pore,som_ec_data$organic_matter)
-print(correlation_pore)
-ggplot(som_ec_data, aes(x = acclima_soil_ec_pore, y = organic_matter)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE) +
-  labs(title = "Regression plot of SOM and Pore EC", x = "Bulk EC", y = "Soil Organic Matter (%)")
-# checking correlation value between Permittivity and SOM 
-correlation_perm <-cor(som_ec_data$acclima_soil_permitivity,som_ec_data$organic_matter)
-print(correlation_perm)
-ggplot(som_ec_data, aes(x = acclima_soil_permitivity, y = organic_matter)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE) +
-  labs(title = "Regression plot of SOM and Permittivity", x = "Permittivity", y = "Soil Organic Matter (%)")
-# two way annova between Bulk EC, SOM, field 
-two.way <- aov(organic_matter ~ acclima_soil_ec_bulk + field, data = som_ec_data)
-summary(two.way)
-# boxplot of field and SOM
-ggplot(som_ec_data, aes(x = field,y = organic_matter, fill = factor(field))) + 
-  geom_boxplot() +
-  labs(title = "Boxplot of Soil Orgainic Matter by Field Type" ,
-       x = "Field Type",
-       y = "Organic Matter") +
-  theme_minimal()
-# boxplot bulk ec and field
-ggplot(som_ec_data, aes(x = field, y = acclima_soil_ec_bulk, fill = factor(field))) +
-  geom_boxplot() +
-  labs(title = "Boxplot of Bulk EC by Field Type",
-       x = "Field Type",
-       y = "Bulk EC") +
-  theme_minimal()
+
 #create seperate tables for corn, soy, and turf
 corn <- som_ec_data %>%
   filter(field=="Corn")  %>%
-  select(field, sample, subsample, acclima_soil_ec_bulk, organic_matter)
+  select(field, sample, subsample, acclima_soil_ec_bulk, acclima_soil_ec_pore,organic_matter)
 soy <- som_ec_data %>% 
   filter(field=='Soy') %>% 
-  select(field, sample, subsample, acclima_soil_ec_bulk, organic_matter)
+  select(field, sample, subsample, acclima_soil_ec_bulk,acclima_soil_ec_pore, organic_matter)
 turf <- som_ec_data %>% 
   filter(field=="Turf")  %>%
-  select(field, sample, subsample, acclima_soil_ec_bulk, organic_matter)
-# correlation corn
+  select(field, sample, subsample, acclima_soil_ec_bulk,acclima_soil_ec_pore,organic_matter)
+
+# box plot of field and SOM
+ggplot(som_ec_data, aes(x = field,y = organic_matter, fill = field)) + 
+  geom_boxplot() + 
+  labs(title = "Soil Organic Matter by Field Type", x = "Field Type", y = "Organic Matter (%)") +
+  theme_minimal()
+
+# box plot Bulk EC and SOM
+ggplot(som_ec_data, aes(x = field, y= acclima_soil_ec_bulk, fill = factor(field))) +
+  geom_boxplot() +
+  labs(title = "Boxplot of Soil Bulk EC by Field Type",
+       x = "Field Type",
+       y = "Soil EC") +
+  theme_minimal()
+
+#checking correlation value between Bulk EC and SOM 
+head(som_ec_data)
+correlation <-cor(som_ec_data$acclima_soil_ec_bulk,som_ec_data$organic_matter)
+print(correlation)
+
+#plot for correlation between Bulk EC and SOM
+ggplot(som_ec_data, aes(x = acclima_soil_ec_bulk, y = organic_matter, color = field, group = 1)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Regression plot of SOM and Bulk EC", x = "Bulk EC", y = "Soil Organic Matter (%)")
+
+#correlation corn organic matter and bulk ec
 correlation_corn <-cor(corn$acclima_soil_ec_bulk,corn$organic_matter)
 print(correlation_corn)
 ggplot(corn, aes(x = acclima_soil_ec_bulk, y = organic_matter)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   labs(title = "Regression plot of SOM and Bulk EC in Corn", x = "Bulk EC", y = "Soil Organic Matter (%)")
-# correlation soy
+
+#correlation soy organic matter and bulk ec
 correlation_soy <-cor(soy$acclima_soil_ec_bulk, soy$organic_matter)
 print(correlation_soy)
 ggplot(soy, aes(x = acclima_soil_ec_bulk, y = organic_matter)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   labs(title = "Regression plot of SOM and Bulk EC in Soy", x = "Bulk EC", y = "Soil Organic Matter (%)")
-# correlation turf
+
+#correlation turf organic matter and bulk ec
 correlation_turf <-cor(turf$acclima_soil_ec_bulk, turf$organic_matter)
 print(correlation_turf)
 ggplot(turf, aes(x = acclima_soil_ec_bulk, y = organic_matter)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   labs(title = "Regression plot of SOM and Bulk EC in Turf", x = "Bulk EC", y = "Soil Organic Matter (%)")
+
+#two way anova between Bulk EC, SOM, field 
+two.way <- aov(organic_matter ~ acclima_soil_ec_bulk + field, data = som_ec_data)
+summary(two.way)
+
+#checking correlation value between EC Pore and SOM
+correlation_pore <-cor(som_ec_data$acclima_soil_ec_pore,som_ec_data$organic_matter)
+print(correlation_pore)
+
+#plot for correlation between Pore EC and SOM 
+ggplot(som_ec_data, aes(x = acclima_soil_ec_pore, y = organic_matter, color = field, group = 1)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Regression plot of SOM and Pore EC", x = "Pore EC", y = "Soil Organic Matter (%)")
+
+#correlation corn organic matter and pore ec
+correlation_corn_pore <-cor(corn$acclima_soil_ec_pore, corn$organic_matter)
+print(correlation_corn_pore)
+ggplot(corn, aes(x = acclima_soil_ec_pore, y = organic_matter)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Regression plot of SOM and Pore EC in Corn", x = "Pore EC", y = "Soil Organic Matter (%)")
+
+#correlation soy organic matter and pore ec
+correlation_soy_pore <-cor(soy$acclima_soil_ec_pore, soy$organic_matter)
+print(correlation_soy_pore)
+ggplot(soy, aes(x = acclima_soil_ec_pore, y = organic_matter)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Regression plot of SOM and Pore EC in Soy", x = "Pore EC", y = "Soil Organic Matter (%)")
+
+#correlation turf organic matter and pore ec
+correlation_turf_pore <-cor(turf$acclima_soil_ec_pore, turf$organic_matter)
+print(correlation_turf_pore)
+ggplot(turf, aes(x = acclima_soil_ec_pore, y = organic_matter)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Regression plot of SOM and Pore EC in Turf", x = "Pore EC", y = "Soil Organic Matter (%)")
+
+#box plot permittivity and field
+ggplot(som_ec_data, aes(x = field, y = acclima_soil_permitivity, fill = field)) +
+  geom_boxplot() +
+  labs(title = "Permittivity by Field Type",
+       x = "Field Type",
+       y = "Permittivity") +
+  theme_minimal()
+
+#checking correlation value between Permittivity and SOM 
+correlation_perm <-cor(som_ec_data$acclima_soil_permitivity,som_ec_data$organic_matter)
+print(correlation_perm)
+
+#plot for correlation between Permittivity and SOM
+ggplot(som_ec_data, aes(x = acclima_soil_permitivity, y = organic_matter, color = field, group = 1)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Correlation of Permittivity and SOM", x = "Permittivity", y = "Soil Organic Matter (%)") +
+  theme_minimal()
+
+#two way anova between permittivity, SOM, and field
+two.way_perm <- aov(organic_matter ~ acclima_soil_permitivity + field, data = som_ec_data)
+summary(two.way_perm)
+
+#checking correlation value between temperature and SOM 
+correlation_temp <-cor(som_ec_data$acclima_soil_temperature,som_ec_data$organic_matter)
+print(correlation_temp)
+
+#plot for correlation between temperature and SOM
+ggplot(som_ec_data, aes(x = acclima_soil_temperature, y = organic_matter, color = field, group = 1)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Comparison of SOM and Temperature", x = "Temperature", y = "Soil Organic Matter (%)") 
+
+#checking correlation value between vwc and SOM
+correlation_vwc <-cor(som_ec_data$acclima_soil_vwc,som_ec_data$organic_matter)
+print(correlation_vwc)
+
+#plot of correlation value between VWC and SOM
+ggplot(som_ec_data, aes(x = acclima_soil_vwc, y = organic_matter, color = field, group = 1)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Regression plot of SOM and VWC", x = "VWC", y = "Soil Organic Matter (%)")
+
+#multiple regression model
+colnames(som_ec_data)
+model = lm(organic_matter ~ acclima_soil_ec_bulk +
+                            acclima_soil_permitivity +
+                            acclima_soil_ec_pore +
+                            acclima_soil_vwc +
+                            acclima_soil_temperature +
+                            field,
+                            
+           data = som_ec_data)
+summary(model)
+
+### MODEL DIAGNOSTICS
+plot(model,1)
+
+ggplot(som_ec_data, aes(x = acclima_soil_permitivity, 
+                        y = organic_matter, color = field, group = 1 )) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE) +
+  labs(title = "Correlation of SOM and Permittivity", 
+       x = "Permittivity", y = "Soil Organic Matter (%)") +
+theme_minimal()
+
+
